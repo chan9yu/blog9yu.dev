578f00a6dcde8b30cf6ce6b562bb1ddeaa979037
chan9yu <dev.cgyeo@gmail.com>
2025-06-21 21:11:45 +0900

    chore: eslint rule 추가구성
    - import 시 type 키워드 필수 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,9 +29,16 @@ const eslintConfig = [
 			"simple-import-sort": simpleImportSort
 		},
 		rules: {
-			"@typescript-eslint/no-unused-vars": "off",
 			"simple-import-sort/imports": "error",
-			"simple-import-sort/exports": "error"
+			"simple-import-sort/exports": "error",
+			"@typescript-eslint/no-unused-vars": "off",
+			"@typescript-eslint/consistent-type-imports": [
+				"error",
+				{
+					prefer: "type-imports",
+					fixStyle: "separate-type-imports"
+				}
+			]
 		}
 	}
 ];
